9de8dce99f287510a787d8134f3447fccd61ee9c
Jonathan Douch <23jd4778@barton.ac.uk>
2026-02-08 04:22:21 +0000

    more functions

--- Quiz/Models.py
+++ Quiz/Models.py
@@ -1,22 +1,51 @@
 from dataclasses import dataclass
 
 class Question:
-    def initQuestion(self, text, multichoice, correct): #constructor to create a question
-        self.text = text
-        self.multichoice = multichoice
-        self.correct = correct 
-        self.answer = None #set after creation of the class
 
-    def checkAnswer(self): # returns a bool true if correct false if otherwise
-        if(self.answer == self.correct):
+    allQuestions = []
+
+    def initQuestion(self, text, answer, difficulty): #constructor to create a question, will get from csv file
+        self.text = text
+        self.answer = answer
+        self.difficulty = difficulty #should be at least 1
+        Question.allQuestions += self
+
+    def checkAnswer(self, useranswer): # returns a bool true if correct false if otherwise
+        if(useranswer == self.answer):
             return True
         else:
             return False
 
+    
+    def  getMaxDifficulty(): #checks every question to get the highest difficulty rating
+        temp = 0
+        for question in Question.allQuestions:
+            if question.difficulty > 0:
+                temp = question.difficulty
+        return temp
+    
+    def getDifficultiesAsList(): #will add each difficulty option to a list for a streamlit cmd to look at
+        difficultylist = []
+
+        count = 0
+        while difficultylist.count() < Question.getMaxDifficulty(): #adds a number to the list
+            count += 1
+            difficultylist += f"Difficulty {count}"
+
+        return difficultylist
+
+
+
 
 class Session:
-    def initSession(self, questions): #constructor to setup Session
-        self.questions = questions
+    
+    sessionQuestions = []
+
+    def initSession(self, userdifficulty): #constructor to setup Session
+        for question in Question.allQuestions: #checks through each question in existence and adds it only if the difficulty level is qual to or less than what is set
+            if(question.difficulty <= userdifficulty):
+                self.sessionQuestions += question
+
     def getScore(self):
         score = sum(2 for question in self.questions if question.checkAnswer) #adds 2 point per correct question, uses function checkAnswer to make sure each question is correct
         return score
--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as sl
 from Quiz.Models import Session, Question
-#from Quiz import
-#from Quiz import 
 
-sl.title("Networking Fundamentals Quiz")
+#run command: cd "D:\Users\jonat\Documents\Personal\Uni\Coding\Computer Science Module\Summative 2 Code"
+#run command: streamlit run app.py
+
+sl.title("Networking Fundamentals Quiz") #title on browser
+sl.write()
+username = sl.text_input("Enter Name: ") #get name and set to username var
+
+
+difficultylist = ["dif 1", "dif 2", "dif 3", "dif 4"]
+difficulty = sl.select_slider("Select Difficulty", difficultylist) #change to Question.getDifficultiesAsList
