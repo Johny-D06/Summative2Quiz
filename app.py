3f8b07792e4288ef3852c36efd2dc321b27fae31
Jonathan Douch <23jd4778@barton.ac.uk>
2026-02-20 00:09:14 +0000

    added more stuff, general save ig

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as sl
 from Quiz.Models import Session, Question
 from Quiz.CSVStorage import CSVWriter
 
-filename = "../QuizData/Questions.csv" #uplevels using .. then uses relative folder and file
+filename = "QuizData/Questions.csv" #uplevels using .. then uses relative folder and file
 
 #run command: cd "D:\Users\jonat\Documents\Personal\Uni\Coding\Computer Science Module\Summative 2 Code"
 #run command: streamlit run app.py
@@ -17,6 +17,9 @@ action = sl.selectbox("Choose Action", ["Start Quiz", "Add Question"])
 if "next" not in sl.session_state:
     sl.session_state.next = False
 
+if "addq" not in sl.session_state:
+    sl.session_state.addq = False
+
 if sl.button("Confirm"):
     sl.session_state.next = True
 
@@ -27,16 +30,20 @@ if sl.session_state.next:
         questiondesc = sl.text_input("Enter Topic", key="t")
 
         if "questionanswers" not in sl.session_state:
-            questionanswers = ["", "", ""]
+            sl.session_state.questionanswers = ["", "", ""]
 
         for i in range(3):
-            questionanswers[i] = (sl.text_input(f"Enter multi choice answer {i+1}: ", key=f"i{i}"))
+            sl.session_state.questionanswers[i] = (sl.text_input(f"Enter multi choice answer {i+1}: ", key=f"i{i}"))
 
-        questioncorrectopt = sl.selectbox("Choose which multi choice answer is correct: ", [questionanswers[0], questionanswers[1], questionanswers[2]], key="c")
+        questioncorrectopt = sl.selectbox("Choose which multi choice answer is correct: ", [sl.session_state.questionanswers[0], sl.session_state.questionanswers[1], sl.session_state.questionanswers[2]], key="c")
 
+    if (sl.button("Add Question")):
+        sl.session_state.addq = True
 
-        Question.addQuestion(csvs, questiontext, questiondesc, questionanswers, questioncorrectopt)
-    else:
-        username = sl.text_input("Enter Name: ") #get name and set to username var
-        # difficultylist = [1, 2, 3, 4] test
-        difficulty = sl.select_slider("Select Difficulty", Question.getDifficultiesAsList) #change to Question.getDifficultiesAsList
+        if (sl.session_state.addq and sl.session_state.next):
+            Question.addQuestion(csvs, questiontext, questiondesc, questionanswers, questioncorrectopt)    
+        
+    #else:
+    #    username = sl.text_input("Enter Name: ") #get name and set to username var
+    #    # difficultylist = [1, 2, 3, 4] test
+    #    difficulty = sl.select_slider("Select Difficulty", Question.getDifficultiesAsList) #change to Question.getDifficultiesAsList
