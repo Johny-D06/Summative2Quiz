9aa6c3250dc426fd2e24f26c9fa51883f133e524
Jonathan Douch <23jd4778@barton.ac.uk>
2026-02-26 23:55:55 +0000

    added further tests, also on csv reader/writer

--- Tests/TestCSVStorage.py
+++ Tests/TestCSVStorage.py
@@ -1,5 +1,11 @@
 import pytest
 from Quiz.CSVStorage import CSVReader, CSVWriter
+from Quiz.Models import Question
 #import classes to test
 
-#def testcsvreader():
+testfilename = "QuizData/QuestionForTest.csv"
+
+def testcsvreader():
+    csvr = CSVReader(testfilename) #intialized
+    csvr.readQuestionFromFile() #csv reader reads the question and creates a question using constructor, this includes adding it to allquestions[]
+    assert Question.allQuestions.count() == 1 #checks a question was made
--- Tests/TestModels.py
+++ Tests/TestModels.py
@@ -3,10 +3,12 @@ from Quiz.Models import Question, Session
 
 def testcheckanswer():
     """checks that the marking works"""
-    question = Question("null", "null", ["null", "null", "correct"], 3, "-1")
+
+    question = Question("null", "null", ["null", "null", "correct"], "correct", "-1") # use 2, ref starts at 0
     assert question.checkAnswer(useranswer="correct") == True
 
 def testgetmaxdifficulty():
-    question = Question("null", "null", ["null", "null", "null"], -1, "2")
-    Question.allQuestions.append(question)
+    """checks getting max difficulty works"""
+
+    question = Question("null", "null", ["null", "null", "null"], "null", "2") #null as not relevant
     assert Question.getMaxDifficulty() == 2
